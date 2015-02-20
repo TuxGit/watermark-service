657fb14bfaca4d76b64204cf2c6228e7c6972410
Ivan Pozdnov <myjobbb@mail.ru>
2015-02-20 13:13:22 +0200

    fixed style.css

--- template/js/main.js
+++ template/js/main.js
@@ -15,5 +15,34 @@ jQuery(document).ready(function(){
 	});
 
 
+	// ivan
+	//выбираю блок с кнопками переключения вида
+	$('.switch-type').on('click','button', function(e){
+			e.preventDefault();
+			var $this = $(this);
+			//реализовую саму подсветку кнопок переключения вида
+			//при клике на кнопку, текущей задаю класс btn--active, а у соседней этот класс убираю
+			$this
+							.addClass('btn--active')
+							.siblings()
+							.removeClass('btn--active');
+						//добавление класса block--hidden, в зависимости от выбранной кнопки 
+						if ($this.hasClass('btn--one') ){
+							$('.position').find('.block--borders').addClass('block--hidden').siblings().removeClass('block--hidden')
+						} else {
+							$('.position').find('.block--coordinates').addClass('block--hidden').siblings().removeClass('block--hidden')
+						} 
+	});
+	//реализую выезжание кнопки социальных сетей
+	$('.socials').find('.social__btn--like').click(function(){
+		$('.socials').toggleClass( 'social--open' );
+	});
+
+	$( ".range-slider" ).slider({
+	range: "min",
+	value: 100,
+	min: 1,
+	max: 100,
+	});
 });
 
