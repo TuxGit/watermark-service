02e5e55263ae3153dc07043a498383b9fc0574f3
Sergey Metelskiy <metelskiisa@mail.ru>
2015-02-26 11:41:51 +0300

    template (with jade errors)

--- app/client/scripts/script.js
+++ app/client/scripts/script.js
@@ -1,4 +1,18 @@
 
 $(document).ready(function () {
-	console.log("Hello World!");
+
+//	КАСТОМИЗАЦИЯ INPUT FILE
+
+	// повесим события клика на input text и img, чтобы срабатывал элемент file  
+
+	$(".upload-block__button, .upload-block__input-text").click(function(){
+	    $(this).parent().children("input[type='file']").trigger("click");
+	});	
+
+	// получаем имя загружаемого файла и помещаем в input text
+
+	$(".upload-block__input-file").change(function(){
+    	$(this).parent().children("input[type='text']").val($(this).val().split('\\').pop());
+	});
+
 });
