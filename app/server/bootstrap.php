c268e83a8c7fbda1c22efa0e685c7aed33d4f55d
Tux <TuxGit@users.noreply.github.com>
2015-03-01 19:56:26 +0300

    add backend core

---
+++ app/server/bootstrap.php
@@ -0,0 +1,19 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+# include settings
+require_once BASEPATH . 'etc/config.php';
+require_once BASEPATH . 'etc/routes.php';
+
+# include core
+require_once BASEPATH . 'modules/core.php';
+require_once BASEPATH . 'controllers/apiCtrl.php';
+
+
+function &get_instance()
+{
+	return App::get_instance();
+}
+
+session_start();
+// global $APP;
+$APP = new App($config, $routes);
--- web/app.php
+++ web/app.php
@@ -2,6 +2,8 @@
 // ini_set('display_errors', 'Off');
 // error_reporting(E_ALL);
 
-define('BASEPATH', __DIR__ . '/');
+define('BASEPATH', __DIR__ . '/../app/server/');
 define('WEBPATH', __DIR__ . '/');
 
+
+require_once BASEPATH . 'bootstrap.php';
